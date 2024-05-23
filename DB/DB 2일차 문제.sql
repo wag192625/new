afa012fe7d374cf6c71280122a9944eee633c4af
wag192625 <wag192625@gmail.com>
2024-05-23 21:01:30 +0900

    안풀리던 문제 해설 및 DB 문제3

--- DB/DB 2일차 문제.sql
+++ DB/DB 2일차 문제.sql
@@ -25,18 +25,14 @@
 -- m VALUES ('D02','전주구장','','','전북 전주시','','');         
 -- m VALUES ('E02','대전월드컵구장','',41000,'대전광역시 유성구 노은동 270번지','','');                                                         
 -- 컵경기장','',63930,'','','');
-
-
-
--- # 각 경기장의 STADIUM_NAME, SEAT_COUNT, TEL을 각각 이름, 좌석수, 전화번호라는 별칭을 사용하여 출력하는 SQL 쿼리를 작성
+  
+-- 각 경기장의 STADIUM_NAME, SEAT_COUNT, TEL을 각각 이름, 좌석수, 전화번호라는 별칭을 사용하여 출력하는 SQL 쿼리를 작성
 -- SELECT STADIUM_NAME as '이름', SEAT_COUNT as '좌석수', TEL as '전화번호' from stadium
 
-
--- # 각 홈팀(HOMETEAM_ID)에 대해 평균 좌석수를 표시하되, 평균 좌석수가 30,000을 초과하는 홈팀만 표시하세요. 결과에는 홈팀 ID와 평균 좌석수를 반올림하여 표시
+-- 각 홈팀(HOMETEAM_ID)에 대해 평균 좌석수를 표시하되, 평균 좌석수가 30,000을 초과하는 홈팀만 표시하세요. 결과에는 홈팀 ID와 평균 좌석수를 반올림하여 표시
 -- SELECT HOMETEAM_ID, round(avg(SEAT_COUNT)) from stadium
 -- where SEAT_COUNT >= 30000
 -- GROUP by HOMETEAM_ID;
-
 
 -- # 각 도시에 있는 경기장의 수를 표시하는 SQL 쿼리를 작성하세요. 결과에는 도시 이름과 경기장 수를 포함
 -- SELECT STADIUM_NAME, count(ADDRESS) from stadium
@@ -50,6 +46,6 @@
 
 -- # 지역 번호가 031인 경기장의 총 좌석수를 계산하는 SQL 쿼리를 작성하세요. 결과에는 해당 지역 번호의 총 좌석수를 포함
 SELECT DDD, sum(SEAT_COUNT) from stadium
-where DDD = 031
+where DDD = '031';
 -- GROUP by SEAT_COUNT
--- HAVING DDD = 031
+-- HAVING DDD = '031'
---
+++ DB/DB 3일차 문제(한빛무역 sql).sql
@@ -0,0 +1,18 @@
+-- 고객 테이블을 조회하시오
+SELECT * from 고객;
+
+--  field7의 데이터가 null 인것만 전체 데이터 추출
+SELECT * from 고객 where field7 is NULL;
+
+--  field1이 EENQU 인 데이터의 field5를 추출
+SELECT field5 from 고객 where field1 = 'EENQU';
+
+--  마일리지가 10000이상인 사람을 조회하고 마일리지 컬럼을 '마일리지총합'이라고 변경하여 추출
+select field2, field9 as '마일리지총합' from 고객 where field9 >= 10000;
+
+--  임의의 행 데이터를 추가.
+--SELECT *, NULL as '임의의 행'  from 고객;
+
+--  field2의 중복되는 데이터가 2개 이상인 데이터를 추출
+-- SELECT field2, count(field2) from 고객
+-- GROUP by field2 HAVING count(field2) >1 ;

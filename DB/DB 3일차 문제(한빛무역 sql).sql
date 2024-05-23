907a0e6ee16e0c6da9241f7ea89c3d8e61ed97f1
wag192625 <wag192625@gmail.com>
2024-05-23 21:47:54 +0900

    한빛무역DB 수정 및 이것저것

--- DB/DB 3일차 문제(한빛무역 sql).sql
+++ DB/DB 3일차 문제(한빛무역 sql).sql
@@ -8,11 +8,12 @@ SELECT * from 고객 where field7 is NULL;
 SELECT field5 from 고객 where field1 = 'EENQU';
 
 --  마일리지가 10000이상인 사람을 조회하고 마일리지 컬럼을 '마일리지총합'이라고 변경하여 추출
+-- 할 때 테이블 field9 int로 바꿔야함
 select field2, field9 as '마일리지총합' from 고객 where field9 >= 10000;
 
 --  임의의 행 데이터를 추가.
---SELECT *, NULL as '임의의 행'  from 고객;
+SELECT *, NULL as '임의의 행'  from 고객;
 
 --  field2의 중복되는 데이터가 2개 이상인 데이터를 추출
--- SELECT field2, count(field2) from 고객
--- GROUP by field2 HAVING count(field2) >1 ;
+SELECT field2, count(field2) from 고객
+GROUP by field2 HAVING count(field2) >1 ;
